7633a0ccd9b362b758835cbeec2143f9cfcbd9ea
suyeon <rlatndus9911@naver.com>
2021-10-19 04:50:53 +0900

    ✨[FEAT] 2차 과제 파일 추가

---
+++ 29th-Assignment/29th-Assignment-Youtube/29th-Assignment-Youtube/Sources/VCs/WelcomeVC.swift
@@ -0,0 +1,55 @@
+//
+//  WelcomeUserViewController.swift
+//  29th-Assignment-Youtube
+//
+//  Created by 김수연 on 2021/10/06.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var otherLoginButton: UIButton!
+    var userName: String?
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUserNameInLabel()
+    }
+    
+    // MARK: - Methods
+    // MARK: Custom Method
+    
+    func setUserNameInLabel() {
+        if let user = userName {
+            welcomeLabel.text = "\(user)님 환영합니다!"
+            welcomeLabel.sizeToFit()
+        }
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func confirmButton(_ sender: Any) {
+        let tabBarStoryboard = UIStoryboard.init(name: "TabBar", bundle: nil)
+        
+        guard let tabbar = tabBarStoryboard.instantiateViewController(withIdentifier: "TabBarController") as? TabBarController else {return}
+        
+        tabbar.modalPresentationStyle = .fullScreen
+        tabbar.modalTransitionStyle = .crossDissolve
+        present(tabbar, animated: true, completion: nil)
+    }
+    
+    @IBAction func otherLoginButtonDidTap(_ sender: Any) {
+        guard let naviVC = self.storyboard?.instantiateViewController(withIdentifier: "NavigationController")
+                as? NavigationController else {return}
+        
+        naviVC.modalPresentationStyle = .fullScreen
+        naviVC.modalTransitionStyle = .crossDissolve
+        self.present(naviVC, animated: true, completion: nil)
+    }
+}
