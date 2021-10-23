f18868210476080cd6a4d66556f40aad3f353833
suyeon <rlatndus9911@naver.com>
2021-10-23 11:12:21 +0900

    ✨[FEAT] 3주차 세미나 프로젝트 파일 생성

---
+++ 29th-Seminar/29th-3week-seminar/29th-3week-seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  29th-3week-seminar
+//
+//  Created by 김수연 on 2021/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
