6a8fabd40bb9ebb7d59ce6cd1deb7622ef7b96ed
suyeon <rlatndus9911@naver.com>
2021-10-31 17:23:46 +0900

    ➕[ADD] 서버통신 연습 프로젝트 생성

---
+++ 29th-Practice/29th-4week-seminar-practice/29th-4week-seminar-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  29th-4week-seminar-practice
+//
+//  Created by 김수연 on 2021/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 29th-Practice/29th-4week-seminar-practice/Pods/Target Support Files/Pods-29th-4week-seminar-practice/Pods-29th-4week-seminar-practice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_29th_4week_seminar_practice : NSObject
+@end
+@implementation PodsDummy_Pods_29th_4week_seminar_practice
+@end
