9ebc0505ddffa23909f9cf2e87893ed7305642ce
suyeon <rlatndus9911@naver.com>
2021-11-02 01:41:55 +0900

    ✨[FEAT] 서버통신 연습 UI 구성 (#2)

--- 29th-Practice/29th-4week-seminar-practice/29th-4week-seminar-practice/ViewController.swift
+++ 29th-Practice/29th-4week-seminar-practice/29th-4week-seminar-practice/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpToLogin(_ sender: Any) {
+    }
+    
 }
 
