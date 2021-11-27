ddf34393021102d4c29257d39e24d71becfd8d10
suyeon <rlatndus9911@naver.com>
2021-11-27 16:47:35 +0900

    7주차 세미나 - 제스쳐

---
+++ 29th-Seminar/29th-7week-seminar/29th-7week-seminar/GestureVC.swift
@@ -0,0 +1,27 @@
+//
+//  GestureVC.swift
+//  29th-7week-seminar
+//
+//  Created by 김수연 on 2021/11/27.
+//
+
+import UIKit
+
+class GestureVC: UIViewController {
+
+    @IBOutlet weak var testView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //제스처 인식기 초기화 및 생성
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapView(gestureRecognizer:)))
+
+        let tapRecognizer2 = UITapGestureRecognizer()
+        tapRecognizer2.addTarget(self, action: #selector(tapView(gestureRecognizer:)))
+
+        testView.addGestureRecognizer(tapRecognizer)
+    }
+
+    @objc func tapView(gestureRecognizer: UIGestureRecognizer){
+        print("뷰안에서 tap")
+    }
+}
