c3e82f557fed36f2633a9a3b4783427e2a2f682b
suyeon <rlatndus9911@naver.com>
2021-12-09 03:32:39 +0900

    codebase 라이브러리 설치

---
+++ 29th-Practice/29th-7week-practice/29th-7week-practice/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  29th-7week-practice
+//
+//  Created by 김수연 on 2021/12/08.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 29th-Practice/29th-7week-practice/Pods/Target Support Files/Pods-29th-7week-practice/Pods-29th-7week-practice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_29th_7week_practice : NSObject
+@end
+@implementation PodsDummy_Pods_29th_7week_practice
+@end
