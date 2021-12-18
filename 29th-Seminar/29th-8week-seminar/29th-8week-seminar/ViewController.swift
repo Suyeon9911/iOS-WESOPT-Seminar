cda59efae236b7a1af584565d6c498c1d92b5a1c
suyeon <rlatndus9911@naver.com>
2021-12-19 07:23:49 +0900

    8차 세미나 실습 코드

--- 29th-Seminar/29th-8week-seminar/29th-8week-seminar/ViewController.swift
+++ 29th-Seminar/29th-8week-seminar/29th-8week-seminar/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
+import Lottie
+
 class ViewController: UIViewController {
+
+    lazy var lottieView: AnimationView = {
+        let animationView = AnimationView(name: "seminar8-sample")
+        animationView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFit
+        animationView.stop()
+        animationView.isHidden = true
+
+        return animationView
+    }()
+
+    var animator: UIViewPropertyAnimator?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(lottieView)
+        //initAnimator()
     }
 
+//    func initAnimator(){
+//        animator = UIViewPropertyAnimator(duration: 3,
+//                                          curve: .easeInOut){
+//            self.testView.frame = CGRect(x: self.view.center.x - 50,
+//                                         y: self.view.center.y - 50,
+//                                         width: 100,
+//                                         height: 100)
+//        }
+//
+//        animator?.addAnimations {
+//            self.testView.backgroundColor = .orange
+//        }
+//
+//        animator?.addCompletion { position in
+//            print(position.rawValue)
+//            print("completion 블록 호출됨")
+//        }
 
+        /// 파라미터 변수를 지정하지 않을 경우.
+        ///
+//        animator?.addCompletion {
+//            print($0.rawValue)
+//            print("")
+//        }
+//
+
+    @IBAction func touchUpToStartAnimation(_ sender: Any) {
+        lottieView.isHidden = false
+        lottieView.play()
+//        animator?.startAnimation()
+    }
+    @IBAction func touchUpToPauseAnimation(_ sender: Any) {
+        lottieView.pause()
+//        animator?.pauseAnimation()
+    }
+
+    @IBAction func touchUpToStopAnimation(_ sender: Any) {
+        lottieView.isHidden = true
+        lottieView.stop()
+//        animator?.stopAnimation(true)
+//
+//        animator?.stopAnimation(false)
+//        animator?.finishAnimation(at: .current)
+    }
 }
 
---
+++ 29th-Seminar/29th-8week-seminar/Pods/Target Support Files/Pods-29th-8week-seminar/Pods-29th-8week-seminar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_29th_8week_seminar : NSObject
+@end
+@implementation PodsDummy_Pods_29th_8week_seminar
+@end
