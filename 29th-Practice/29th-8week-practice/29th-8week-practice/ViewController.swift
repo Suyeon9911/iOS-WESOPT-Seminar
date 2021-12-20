0491cf8c01287d63b873e930a9f8e2198ebff43a
suyeon <rlatndus9911@naver.com>
2021-12-20 20:56:56 +0900

    8차 세미나 연습 프로젝트 생성

---
+++ 29th-Practice/29th-8week-practice/29th-8week-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  29th-8week-practice
+//
+//  Created by 김수연 on 2021/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
